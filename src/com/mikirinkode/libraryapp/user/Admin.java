e788e266757e7f7c709a927020a8f1b5a9fe04df
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-09 16:02:35 +0700

    updating method addNewBook()

--- src/com/mikirinkode/libraryapp/user/Admin.java
+++ src/com/mikirinkode/libraryapp/user/Admin.java
@@ -2,21 +2,36 @@ package com.mikirinkode.libraryapp.user;
 
 import com.mikirinkode.libraryapp.library.Book;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 // inherit kelas User
 public class Admin extends User {
+    // inisialisasi kelas scanner untuk Input User
+    private Scanner input = new Scanner(System.in);
 
     // constructor kelas Admin yang memanggil constructor kelas Parent nya
     public Admin(String fullName, String username, String password) {
         super(fullName, username, password, "Admin");
     }
 
-    void updateBookProperties(Book book, String title, String category, String authorName, int releaseDate) {
+    public void updateBookProperties(Book book, String title, String category, String authorName, int releaseDate) {
         book.updateDetail(title, category, authorName, releaseDate);
         System.out.println("Buku " + title + " berhasil diperbaharui");
     }
 
-    void addNewBook(String bookTitle, String bookCategory, String authorName, int releaseDate) {
-        Book book = new Book(bookTitle, bookCategory, authorName, releaseDate);
-        System.out.println("Berhasil menambahkan " + bookTitle + " ke data Perpustakaan.");
+    public void addNewBook(ArrayList<Book> libraryBooks) {
+        System.out.print("Judul Buku: ");
+        String bookTitle = input.nextLine();
+        System.out.print("Kategori: ");
+        String bookCategory = input.nextLine();
+        System.out.print("Penulis: ");
+        String authorName = input.nextLine();
+        System.out.print("Tahun Rilis: ");
+        int releaseDate = input.nextInt();
+        input.nextLine();
+
+        libraryBooks.add(new Book(bookTitle, bookCategory, authorName, releaseDate));
+        System.out.println("Berhasil menambahkan buku \"" + bookTitle + "\" ke data Perpustakaan.");
     }
 }
