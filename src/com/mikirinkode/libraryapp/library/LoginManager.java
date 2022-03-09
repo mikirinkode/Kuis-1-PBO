efa87b261c4f7bdfbb25d18c22266e3571f68dd7
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-09 16:06:54 +0700

    fix bug looping

--- src/com/mikirinkode/libraryapp/library/LoginManager.java
+++ src/com/mikirinkode/libraryapp/library/LoginManager.java
@@ -17,6 +17,7 @@ import java.util.Scanner;
  */
 public final class LoginManager {
     private static final Scanner input = new Scanner(System.in);
+    private static boolean menuActiveStatus = true;
 
     // private constructor agar tidak dapat dibuat objek
     private LoginManager(){
@@ -28,25 +29,21 @@ public final class LoginManager {
         tanpa membuat objeknya dulu
      */
     public static void menu(Library library) {
-        System.out.println("1. Login");
-        System.out.println("2. Daftar");
-        System.out.print("Pilihan [1/2]: ");
-
-        while (true) {
+        while (menuActiveStatus) {
+            System.out.println("1. Login");
+            System.out.println("2. Daftar");
+            System.out.print("Pilihan [1/2]: ");
             try {
                 int userChoice = input.nextInt();
                 input.nextLine();  // untuk ambil input hingga akhir baris pada nextInt sebelumnya
                 switch (userChoice) {
                     case 1 -> login(library);
                     case 2 -> signup(library);
-                    default -> System.out.println("Invalid Input!");
+                    default -> System.out.println("Invalid Input! \n");
                 }
-                break; // keluar dari loop setelah berhasil login atau signup
+                break; // biar loop nya berhenti, tpi klo gagal login nanti bakal tetep loop
             } catch (InputMismatchException e) {
                 System.out.println("Invalid Input! \n");
-                System.out.println("1. Login");
-                System.out.println("2. Daftar");
-                System.out.print("Pilihan [1/2]: ");
                 input.nextLine(); // meminta input lagi, jika sebelumnya invalid
             }
         } // akhir while loop
@@ -54,6 +51,7 @@ public final class LoginManager {
 
 
     private static void login(Library library) {
+        boolean loginSuccess = false;
         System.out.println("\nSilahkan login terlebih dahulu.");
         System.out.print("Username: ");
         String username = input.next();
@@ -63,23 +61,29 @@ public final class LoginManager {
         // validasi login
         for (Admin admin : library.getAdminList()) {
             if (admin.getUsername().equals(username) && admin.getPassword().equals(password)) {
+                library.setLoggedAdmin(admin); // menetapkan admin yang login
                 library.adminMenu();
+                menuActiveStatus = false;   // keluar dari loop pada method menu LoginManager
+                loginSuccess = true;
                 break;
             } else if (!library.getMemberList().isEmpty()) { // jika user bukan admin dan memberListtidak kosong, maka cek memberList
                 // looping setiap objek member yang telah dibuat
                 for (LibraryMember member : library.getMemberList()) {
                     // mengecek username dan password
                     if (member.getUsername().equals(username) && member.getPassword().equals(password)) {
+                        library.setLoggedMember(member); // menetapkan user yang login
                         library.memberMenu();
+                        menuActiveStatus = false;   // keluar dari loop pada method menu LoginManager
+                        loginSuccess = true;
                         break;
                     }
                 } // end for loop
-            } else { // jika bukan admin dan member, maka login invalid
-                System.out.println("Username or Password Invalid.\n");
-                menu(library);
-                break;
             }
-        } // end for loop
+        }
+        if (!loginSuccess) { // jika gagal login
+            System.out.println("Username or Password Invalid.\n");
+            menu(library);
+        }// end for loop
     }
 
     private static void signup(Library library) {
