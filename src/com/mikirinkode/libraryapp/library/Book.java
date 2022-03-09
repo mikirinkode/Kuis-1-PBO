122d742419222f77741de3dfafe1d4c996e153c0
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-03-09 16:03:14 +0700

    updating bookId

--- src/com/mikirinkode/libraryapp/library/Book.java
+++ src/com/mikirinkode/libraryapp/library/Book.java
@@ -1,20 +1,21 @@
 package com.mikirinkode.libraryapp.library;
 
+
+
 public class Book {
     private final String bookId;
     private String bookTitle;
     private String bookCategory;
     private String authorName;
     private int releaseDate;
-    private int idCounter = 0;
+    private static long idCounter = 0;
 
     public Book(String bookTitle, String bookCategory, String authorName, int releaseDate) {
         this.bookTitle = bookTitle;
         this.bookCategory = bookCategory;
         this.authorName = authorName;
         this.releaseDate = releaseDate;
-        this.bookId = "0" + idCounter;
-        this.idCounter++;
+        this.bookId = "0" + createId();
     }
 
     public void updateDetail(String bookTitle, String bookCategory, String authorName, int releaseDate){
@@ -25,11 +26,22 @@ public class Book {
     }
 
     void printBookDetail() {
-        System.out.println("ID Buku \t: " + bookId);
-        System.out.println("Nama Buku \t: " + bookTitle);
+        System.out.println("ID Buku \t\t: " + bookId);
+        System.out.println("Nama Buku \t\t: " + bookTitle);
         System.out.println("Kategori Buku \t: " + bookCategory);
         System.out.println("Nama Penulis \t: " + authorName);
         System.out.println("Tanggal Rilis \t: " + releaseDate);
+    }
+
+    private static String createId(){
+        return String.valueOf(idCounter++);
+    }
+
+    /*
+        Getter Setter
+     */
+    public String getBookId() {
+        return bookId;
     }
 
     public String getBookTitle() {
@@ -39,6 +51,15 @@ public class Book {
     public void setBookTitle(String bookTitle) {
         this.bookTitle = bookTitle;
     }
+
+    public String getBookCategory() {
+        return bookCategory;
+    }
+
+    public void setBookCategory(String bookCategory) {
+        this.bookCategory = bookCategory;
+    }
+
 
     public int getReleaseDate() {
         return releaseDate;
@@ -56,11 +77,4 @@ public class Book {
         this.authorName = authorName;
     }
 
-    public String getbookCategory() {
-        return bookCategory;
-    }
-
-    public void setbookCategory(String bookCategory) {
-        this.bookCategory = bookCategory;
-    }
 }
